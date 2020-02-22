8575e2b9cd4277a21df32d38afcfe2b76f01776f
shoya baba <epifa_kikka@icloud.com>
2020-02-22 06:40:09 +0000

    Create relations table

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,11 @@ class User < ApplicationRecord
   validates :introduce, presence: true, length: { maximum: 255 }
   
   has_secure_password
+  
+  has_many :relationships
+  has_many :following, through: :relationships, source: :follow
+  has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :followers, through: :reverses_of_relationship, source: :user
+  
   mount_uploader :picture, ImageUploader
 end
